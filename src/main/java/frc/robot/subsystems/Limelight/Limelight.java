14a6b61c5c2c7d9fa5eb634e734cecf1b93cb5e1
Mysterium422 <mysterium422@gmail.com>
2024-10-05 03:39:46 -0400

    Moves megatag to limelight subsystem

--- src/main/java/frc/robot/subsystems/Limelight/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight/Limelight.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Limelight.LimelightIO.LimelightIO;
 import frc.robot.subsystems.Limelight.LimelightIO.LimelightIOInputsAutoLogged;
 
@@ -34,8 +37,18 @@ public class Limelight extends SubsystemBase{
 
    @Override
    public void periodic() {
-       limelightIO.updateInputs(limelightIOInputs);
-       Logger.processInputs("Limelight", limelightIOInputs);
+     limelightIO.updateInputs(limelightIOInputs);
+     Logger.processInputs("Limelight", limelightIOInputs);
+   }
+
+   public Pose2d getMegatagPose(Pose2d estimatedPose) {
+     LimelightHelpers.SetRobotOrientation("limelight", estimatedPose.getRotation().getDegrees(), 0, 0, 0, 0, 0);
+     LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+     if (mt2 == null || mt2.pose == null || mt2.tagCount == 0) return null;
+     
+     Pose2d invertedMt2Pose = new Pose2d(new Translation2d(-mt2.pose.getX(), mt2.pose.getY()), mt2.pose.getRotation());
+
+     return invertedMt2Pose;
    }
    
 }
