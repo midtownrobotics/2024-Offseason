d496fde227a269ca21d1547938478d49e9db6dce
Woody Spivey <woodyspivey@gmail.com>
2024-10-26 19:58:14 -0400

    don't just setRumble every other cycle, set it only when it's different

--- src/main/java/frc/robot/utils/IOProtectionXboxController.java
+++ src/main/java/frc/robot/utils/IOProtectionXboxController.java
@@ -3,17 +3,15 @@ package frc.robot.utils;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class IOProtectionXboxController extends XboxController{
-    public boolean rumbleLastCycle;
+    public double currentRumbleValue;
     public IOProtectionXboxController(int port) {
         super(port);
     }
 
     public void setRumble(RumbleType rumbleType, double value) {
-        if (!rumbleLastCycle) {
+        if (currentRumbleValue != value) {
             super.setRumble(rumbleType, value);
-            rumbleLastCycle = true;
-        } else {
-            rumbleLastCycle = false;
+            currentRumbleValue = value;
         }
     }
 }
