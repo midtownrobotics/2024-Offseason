600a26bb731944c14337090eb6194a6017b588bb
Gray JN <me@grayjn.com>
2024-11-09 22:12:13 -0500

    added cos compensation :-)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 204;
-  public static final String GIT_SHA = "d7243b7c1c97d3381ea8662f67d91dc0669ab0bb";
-  public static final String GIT_DATE = "2024-10-26 01:54:30 GMT";
-  public static final String GIT_BRANCH = "63-update-revctre-classes";
-  public static final String BUILD_DATE = "2024-10-28 23:30:20 GMT";
-  public static final long BUILD_UNIX_TIME = 1730158220273L;
+  public static final int GIT_REVISION = 230;
+  public static final String GIT_SHA = "3b6b863f4d095757f2dc8eeea0711ae607e8075d";
+  public static final String GIT_DATE = "2024-11-09 16:42:34 GMT";
+  public static final String GIT_BRANCH = "55-implement-cosine-compensation";
+  public static final String BUILD_DATE = "2024-11-10 03:10:33 GMT";
+  public static final long BUILD_UNIX_TIME = 1731208233761L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
